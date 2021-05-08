f04c3b43b44a743bfdb293d429167e268b92c7eb
venkatasrikanthm <34295443+venkatasrikanthm@users.noreply.github.com>
2021-05-08 12:25:54 +0530

    build configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBdkoTte3fLUI3JI4SRv7Y2m8FC-ouOvl8',
+    authDomain: 'mircoshopportals.firebaseapp.com',
+    projectId: 'mircoshopportals',
+    storageBucket: 'mircoshopportals.appspot.com',
+    messagingSenderId: '132342370514',
+    appId: '1:132342370514:web:f037756a4933db90c4dd0b',
+    measurementId: 'G-EEP03YVT40'
+  },
   GoogleAnalyticsId: ''
 };
