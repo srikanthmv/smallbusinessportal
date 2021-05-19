a0b0582f0c6836798b8c777914c7520f0b13e002
venkatasrikanthm <34295443+venkatasrikanthm@users.noreply.github.com>
2021-05-19 23:25:10 +0530

    added login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthenticationService} from '../services/authentication.service';
-import {UserLoginModel} from '../models/user-login.model';
-import {EmailValidator, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,37 +6,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  userLoginInfoFg: FormGroup = new FormGroup({});
-  constructor(private authenticationService: AuthenticationService, private fb: FormBuilder,
-              private fAuth: AngularFireAuth, private router: Router) {
-    this.userLoginInfoFg = this.fb.group({
-      username: [null, [Validators.required, new EmailValidator()]],
-      password: [null, Validators.required]
-    });
-  }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.fAuth.authState.subscribe(user => {
-      if (user){
-        localStorage.setItem('$token', JSON.stringify(user));
-        this.router.navigate(['/admin/dashboard']);
-      } else {
-        localStorage.setItem('$token', '');
-      }
-    });
   }
 
-  get username(): FormControl {
-    return this.userLoginInfoFg.get('username') as FormControl;
-  }
-
-  get password(): FormControl {
-    return this.userLoginInfoFg.get('password') as FormControl;
-  }
-
-  loginWithAdmin(): void {
-    if (this.userLoginInfoFg?.valid) {
-      this.authenticationService.loginAdminUser(this.userLoginInfoFg.value);
-    }
-  }
 }
