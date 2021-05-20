88ca2ffccb239a448f37aa6a86540ca2e82c18ce
venkatasrikanthm <34295443+venkatasrikanthm@users.noreply.github.com>
2021-05-20 15:31:00 +0530

    removed un used icons and added new baner image on homepage

--- src/app/slide-show/slide-show.component.ts
+++ src/app/slide-show/slide-show.component.ts
@@ -12,9 +12,7 @@ export class SlideShowComponent implements OnInit {
 
   ngOnInit(): void {
     this.offerImages = [
-      new ImageItem({ type: 'image', src: 'assets/slideshow-images-af/137538189_105039234893862_4964641684504420014_n.jpg' }),
-      new ImageItem({ type: 'image', src: 'assets/slideshow-images-af/139639298_107871964610589_1691726782151187683_o.jpg' }),
-      new ImageItem({ type: 'image', src: 'assets/slideshow-images-af/139906964_108005237930595_5625715771528140237_o.jpg'})]
+      new ImageItem({ type: 'image', src: 'assets/slideshow-images-af/offer-image.jpg' })]
   }
 
 }
